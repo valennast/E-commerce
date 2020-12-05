cbcded697378bc01e44cdb0d9f4a02044ace745d
valentin astudillo <gvalentin.ast@gmail.com>
2020-12-05 14:40:42 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,58 @@
+class Card {
+    constructor(name,price,img){
+        this.name = name;
+        this.price = price;
+        this.img = img;
+        this.id = Math.random()*1000;
+    }
+}
+
+var cards;
+window.onload= () => {
+    let objectFromJson = JSON.parse(dbJson);
+    /* console.log(objectFromJson); */
+    cards = objectFromJson.map(object =>{
+        return new Card(object.name,object.price,object.imgPath)
+    });
+    /* console.log(cards); */
+
+    let cardContainer = document.getElementById("foodcards");
+    /* console.log(cardContainer) */
+    cards.forEach(element => {
+        cardContainer.innerHTML+= createCard(element);
+    });
+}
+
+function createCard(card){
+    return `<div class="card">
+          <img src="${card.img}" class="card-img-top">
+          <div id="${card.id}" class="card-body">
+            <h5 class="card-title">${card.name}</h5>
+            <p class="card-text"> // Price $${card.price}</p>   
+            <a href="#" class="btn btn-warning" onclick="addCard(event)"><span><i class="fas fa-cart-plus "></i></span>Add to the car </a>
+          </div>
+          <div class="card-footer">
+            <small class="text-muted"></small>
+          </div>
+        </div>`
+
+}
+
+function addCard(event){
+    let counter = document.getElementById("shopcar");
+    let total_purchase=document.getElementById("total_purchase");
+    let currentvalue = counter.innerHTML;
+    counter.innerHTML = parseInt(currentvalue) + 1;
+ /*    console.log (event.target.parentNode); */
+    let container = event.target.parentNode;
+/*     cards.forEach(element => {
+        if(container.id == element.id) console.log(element);
+    }); */
+    let product= cards.find(element => {
+        return container.id == element.id
+    });
+    /* console.log(total_purchase.innerHTML); */
+    let current_total=total_purchase.innerHTML;
+    total_purchase.innerHTML = parseFloat(current_total) + product.price;
+    /* console.log(product); */
+}
---
+++ js/db.js
@@ -0,0 +1,6 @@
+var dbJson=`[
+    {"name" : "Producto 1" , "price" : 500, "imgPath" : "img/img1.jpg"},
+    {"name" : "Producto 2" , "price" : 1500, "imgPath" : "img/img2.jpg"},
+    {"name" : "Producto 3" , "price" : 1000, "imgPath" : "img/img3.jpg"},
+    {"name" : "Producto 4" , "price" : 200, "imgPath" : "img/img4.jpg"},
+    {"name" : "Producto 5" , "price" : 750, "imgPath" : "img/img5.jpg"}]`;
